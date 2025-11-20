9bebae5838cf75663b67a4a994af98ea10ec51bc
Vytalithy <Vytalithy@tekn.website>
2025-11-20 22:39:21 +0000

    Create stackbit.config.ts

---
+++ stackbit.config.ts
@@ -0,0 +1,7 @@
+import { defineStackbitConfig } from "@stackbit/types";
+export default defineStackbitConfig({
+ stackbitVersion: "~0.6.0",
+ contentSources: [
+ ],
+ modelExtensions: [{ name: "page", type: "page", urlPath: "/{slug}" }]
+});
